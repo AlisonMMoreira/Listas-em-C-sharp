9e8345e1a7e1e7cc99b00242f2f833f2b9d05c34
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-01-22 23:07:11 -0300

    Update AprendendoListas.cs

--- AprendendoListas.cs
+++ AprendendoListas.cs
@@ -24,3 +24,37 @@ namespace AprendendoListas
         }
     }
 }
+
+// Uma das várias formas de ler as informações de uma lista criada
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clientes = new List<string>();
+            clientes.Add("Yuri");
+            clientes.Add("André");
+            clientes.Add("marcelo");
+            clientes.Add("mateus");
+            string fornecedor = "guilherme prata";
+            clientes.Add(fornecedor);
+
+            Console.WriteLine("Valores foram inseridos :)");
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}

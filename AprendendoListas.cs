6dfe63572a190d07fc6a4318c96cf98a52a13732
Alison Moreira <120423515+AlisonMMoreira@users.noreply.github.com>
2023-01-22 23:18:07 -0300

    Update AprendendoListas.cs

--- AprendendoListas.cs
+++ AprendendoListas.cs
@@ -58,3 +58,39 @@ namespace AprendendoListas
         }
     }
 }
+
+// 2ª forma de exibir um determinado conteúdo de dentro da lista, é usando a função de tamanho, exemplo.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clientes = new List<string>();
+            clientes.Add("Yuri");
+            clientes.Add("André");
+            clientes.Add("marcelo");
+            clientes.Add("mateus");
+            string fornecedor = "guilherme prata";
+            clientes.Add(fornecedor);
+
+
+            //na linha abaixo é possível ver que o <code>FindAll</code> consegue filtrar os dados que conhetam até o tamanho determinado pelo desenvolvedor.
+          List<string> filtrada = clientes.FindAll(cliente => cliente.Length < 5);
+          Console.WriteLine("=====================");
+
+          foreach (string cliente in filtrada)
+          {
+            Console.WriteLine(cliente);
+          }
+
+    }
+  }
+}
